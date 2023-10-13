7aac6417d97e5c3be9b4ab6333c240898cd4a781
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-10-13 22:33:33 +0530

    app initialised with tailwind framework

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Signup from "./components/Signup"
+import Appbar from "./components/Appbar"
+function App() {
+  
+
+  return (
+    <div><h2 className="text-3xl font-bold underline">
+    Hello world!
+  </h2>
+  <Signup></Signup>
+  <Appbar></Appbar>
+ </div>
+  )
+}
+
+export default App
---
+++ src/components/Appbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Appbar = () => {
+  return (
+    <div>hello from Appbar</div>
+  )
+}
+
+export default Appbar
