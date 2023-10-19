29578ae61e771723edc820d473b3fa498b399d94
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-10-19 19:50:07 +0530

    Add course admin working

--- src/components/AddCourse.tsx
+++ src/components/AddCourse.tsx
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const AddCourse = () => {
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
-  const [image, setImage] = useState('');
+  const [imageLink, setImageLink] = useState('');
   const [price, setPrice] = useState('');
 
+  const navigate = useNavigate();
 
 //  COMPONENET TO CREATE NEW COURSE BY ADMIN
 
@@ -15,13 +17,14 @@ const AddCourse = () => {
     
     <div className="
     w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 my-10 mx-auto">
-    <form className="space-y-6" action="#">
+    <div className="space-y-6">
         <h5 className="text-xl font-medium text-gray-900 dark:text-white">Create new course</h5>
         <div>
             <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
             <input 
             onChange={(e)=>{
                 setTitle(e.target.value);
+                console.log("title:",e.target.value);
             }}
              type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="title name" required />
         </div>
@@ -30,6 +33,7 @@ const AddCourse = () => {
             <textarea 
             onChange={(e)=>{
               setDescription(e.target.value);
+              console.log("Descr:",e.target.value);
           }}
             name="description" id="description" placeholder="course description here" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
         </div>
@@ -37,7 +41,8 @@ const AddCourse = () => {
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Image</label>
             <input
             onChange={(e)=>{
-              setImage(e.target.value);
+              setImageLink(e.target.value);
+              console.log("ImageLink:",e.target.value);
           }}
             type="text" name="image" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="image url" required />
         </div>
@@ -46,6 +51,7 @@ const AddCourse = () => {
             <input 
             onChange={(e)=>{
               setPrice(e.target.value);
+              console.log("price:",e.target.value);
           }}
             type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" placeholder="amount" required />
         </div>
@@ -55,13 +61,21 @@ const AddCourse = () => {
           axios.post('http://localhost:8000/admin/courses',{
             title : title,
             description : description,
-            image : image,
+            imageLink : imageLink,
             price : price
+          }, {
+            headers : { 'authorization': 'Bearer ' + localStorage.getItem('token') 
+            }
           })
         }}
         type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create new course</button>
+
+<button onClick={()=>{
+  navigate('/welcomeuser');
+}} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">BACK TO WELCOME PAGE</button>
         
-    </form>
+    </div>
+    
 </div>
 
 
