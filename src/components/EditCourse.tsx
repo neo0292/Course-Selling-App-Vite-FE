774dc6823e640f7c0977c7aaffa6bcdc3744b8c4
neo0292 <110742271+neo0292@users.noreply.github.com>
2023-10-21 13:30:37 +0530

    edit course card working

--- src/components/EditCourse.tsx
+++ src/components/EditCourse.tsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 
 const EditCourse = () => {
   const {courseId} = useParams();
-  console.log('courseid from useParams:',courseId)
-  const [course, setCourse] = useState();
+  const [course, setCourse] = useState(null);
 
   useEffect(() => {
     async function fetchCourse() {
@@ -16,9 +15,8 @@ const EditCourse = () => {
             authorization: 'Bearer ' + localStorage.getItem('token')
           }
         });
-
         const data = response.data;
-        setCourse(data.courses);
+        setCourse(data.course); 
         console.log("Courses recived successfully:", data.course);
       } catch (error) {
         console.error('Error fetching data:', error);
@@ -29,15 +27,124 @@ const EditCourse = () => {
     fetchCourse();
   }, []);
   
-    
+  if (!course) {
+    console.log('course from if:', course )
+    return <div style={{height: "100vh", justifyContent: "center", flexDirection: "column"}}>
+        Loading....
+    </div>
+  } 
   
-  
-
-
-
   return (
-    <div>hekllo{course}</div>
+    <>
+    <Topbar course = {course}></Topbar>
+    <EditCard course = {course} setCourse = {setCourse}></EditCard>
+    <DisplayCard></DisplayCard>
+    </>
   )
 }
+
+const Topbar =({course}) => {
+  return (
+    <>
+    <div className='flex flex-row  justify-center items-center h-20 w-screen bg-sky-300'>{course.title}</div>  
+  </>
+  )
+
+
+}
+
+
+const EditCard = ({course,setCourse}) => {
+  const [title, setTitle] = useState(course.title);
+  const [description, setDescription] = useState(course.description);
+  const [imageLink, setImageLink] = useState(course.imageLink);
+  const [price, setPrice] = useState(course.price);
+
+  const navigate = useNavigate();
+  return (
+    <>
+    <div className="flex flex-col h-screen bg-gradient-to-r from-indigo-200 via-indigo-400 to-indigo-800">
+    
+    <div className="
+    w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 my-10 mx-auto">
+    <div className="space-y-6">
+        <h5 className="text-xl font-medium text-gray-900 dark:text-white">Edit this course</h5>
+        <div>
+            <label  className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Title</label>
+            <input 
+            onChange={(e)=>{
+                setTitle(e.target.value);
+                console.log("title:",e.target.value);
+            }}
+            type="text" name="title" id="title" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"  value={title} required />
+        </div>
+        <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
+            <textarea 
+            onChange={(e)=>{
+              setDescription(e.target.value);
+              console.log("Descr:",e.target.value);
+          }}
+            name="description" id="description" value={description} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" required />
+        </div>
+        <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">imageLink</label>
+            <input
+            onChange={(e)=>{
+              setImageLink(e.target.value);
+              console.log("ImageLink:",e.target.value);
+          }}
+            type="text" name="image" id="image" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value={imageLink} required />
+        </div>
+        <div>
+            <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">price</label>
+            <input 
+            onChange={(e)=>{
+              setPrice(e.target.value);
+              console.log("price:",e.target.value);
+          }}
+            type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" value={price} required />
+        </div>
+     
+        <button 
+        onClick={async() =>{
+          axios.put('http://localhost:8000/admin/courses/' + course._Id,{
+            title : title,
+            description : description,
+            imageLink : imageLink,
+            price : price
+          }, {
+            headers : { 'authorization': 'Bearer ' + localStorage.getItem('token') 
+            }
+          })
+          setCourse(
+            course.title=title,
+            course.description=description,
+            course.imageLink=imageLink,
+            course.price=price)
+        }}
+        type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Update course</button>
+
+      <button onClick={()=>{
+      navigate('/welcomeuser');
+      }} type="submit" className="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">BACK TO WELCOME PAGE</button>
+        
+    </div>
+    
+  </div>
+</div>
+</>
+)
+}
+
+
+
+
+const DisplayCard = () => {
+  return (
+    <div>displayCard</div>
+  )
+}
+
 
 export default EditCourse
